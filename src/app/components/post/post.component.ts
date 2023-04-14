8544b563eeefcecd92035c349220f7c22ea7cb21
Thiago Rubin <rubinthiago@gmail.com>
2023-04-14 14:07:02 -0300

    project new

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+	tweetMessage:string = ""
+	tweetMaxLenght: number = 240
+	tweetRemain:number = this.tweetMaxLenght
+
+	sendTweet(){
+		alert("twitter criado")
+	}
+
+	changeTweet(){
+		this.tweetRemain = 240 - this.tweetMessage.length
+	}
+}
